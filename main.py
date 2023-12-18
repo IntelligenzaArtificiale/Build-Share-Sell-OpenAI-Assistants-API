eaa2df5474e6ba05f6f97cfb79d6f6a48e3a2bdd
IntelligenzaArtificiale <ciciarelli.1897655@studenti.uniroma1.it>
2023-12-18 15:40:49 +0100

    Fix temporary file creation bug

--- main.py
+++ main.py
@@ -49,7 +49,9 @@ if openaiKey:
             if len(file_up) > 1:
                 for file in file_up:
                     suffix = file.name.split(".")[-1]
-                    with NamedTemporaryFile(delete=False,dir='.', suffix=f".{suffix}") as tmp_file:
+                    st.write(suffix)
+                    time.sleep(2)
+                    with NamedTemporaryFile(dir='.', suffix=f".{suffix}") as tmp_file:
                         tmp_file.write(file.read())
                         st.write(tmp_file.name)
 
