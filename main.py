9b3c35086f91f987645ca4eba550799afe19b60a
IntelligenzaArtificiale <ciciarelli.1897655@studenti.uniroma1.it>
2023-12-19 12:34:41 +0100

    Fix formatting in main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import os
 import zipfile
 import yaml
-
+ 
 from inference_assistant import inference
 from utils import create_assistant_from_config_file, upload_to_openai, export_assistant
 
